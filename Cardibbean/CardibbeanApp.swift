f496387a1a74dc8786e818d073d190f13738f214
Cristian Diaz <xtndiaz@gmail.com>
2022-07-05 10:30:05 -0500

    Initial Commit

---
+++ Cardibbean/CardibbeanApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardibbeanApp.swift
+//  Cardibbean
+//
+//  Created by Cristian Diaz on 5.7.2022.
+//
+
+import SwiftUI
+
+@main
+struct CardibbeanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
